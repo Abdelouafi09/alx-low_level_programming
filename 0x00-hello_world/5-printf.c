e9bafa55962bccba9a6612627816e5367737de39
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-17 00:15:27 +0000

    fifth 2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -11,4 +11,3 @@ int main(void)
 		printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
-
