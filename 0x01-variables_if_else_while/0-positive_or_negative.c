eeca37d2de89ac4850cce5eba81edec7411438b1
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-17 22:19:40 +0000

    first task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -16,7 +16,7 @@ int main(void)
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%d is zero\n" ,n);
+		prinf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
 
