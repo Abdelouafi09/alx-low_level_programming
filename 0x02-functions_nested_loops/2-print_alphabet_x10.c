8573457ba7f5b6884244f61199216be4d5271489
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-22 01:57:24 +0000

    third

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - show the alphabet 10 times
+ *
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char j;
+
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
