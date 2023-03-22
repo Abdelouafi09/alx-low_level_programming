e0bf5efa848cdafd9179e8783d2e468a3a04932e
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-22 23:44:34 +0000

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - return the suthe sum of two int
+ *
+ * @a: input to operates on
+ * @b: second input
+ * Return: return the addition result of a and b
+ *
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
