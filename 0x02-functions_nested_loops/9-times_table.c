319f2c3dfcef1404b82a21b4980802e8e713de41
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-22 05:24:36 +0000

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - return all minutes
+ *
+ *
+ *
+ */
+
+void times_table(void)
+{
+	int i, j, r;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			r = i * j;
+			if (j == 0)
+			{
+				_putchar(0 + '0');
+			}
+			else if(r >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(r / 10 + '0');
+				_putchar(r % 10 + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(r % 10 + '0');
+			}
+		}
+		_putchar('\n');
+
+	}
+}
