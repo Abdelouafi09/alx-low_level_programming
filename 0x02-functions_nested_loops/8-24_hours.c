95a760a4a4596744f8c8881226690755e549be2e
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-22 03:57:01 +0000

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,13 +11,15 @@ void jack_bauer(void)
 {
 	int h, m;
 
-	for (h = 100 % 100; h < 24; h++)
+	for (h = 0; h < 24; h++)
 	{
-		for (m = 100 % 100; m < 60; m++)
+		for (m = 0; m < 60; m++)
 		{
-			_putchar(h + '0');
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
 			_putchar(':');
-			_putchar(m + '0');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
 			_putchar('\n');
 		}
 	}
