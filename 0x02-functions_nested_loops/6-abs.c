d63dbbad6a785628b87cf23218f9278863c3bbaf
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-22 02:44:13 +0000

    seventh

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - give the absolute value of an int
+ *
+ * Return: +x if x < 0 and x if x > 0
+ *
+ * @x: character given to the function
+ */
+int _abs(int x)
+{
+	if (n < 0)
+	{
+		int r;
+
+		r = -(x);
+		return (x);
+	}
+	else
+		return (x);
+}
