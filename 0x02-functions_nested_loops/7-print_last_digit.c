e559a59e2d1282fa763046c2712b1a0548401121
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-22 02:50:01 +0000

    8

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - return last digit
+ *
+ * Return: 1 if 
+ *
+ * @n: character given to the function
+ */
+int print_sign(int n)
+{
+	int r;
+
+	r = n % 10;
+	return (r);
+}
