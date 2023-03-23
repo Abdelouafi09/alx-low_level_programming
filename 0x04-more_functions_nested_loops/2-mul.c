5791a745bb6972597f5879792166f8a954493809
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-23 15:33:04 +0000

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * mul - multiply two integers
+ *
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result of multiplying a and b
+ */
+int  mul(int a, int b)
+{
+	return (a * b);
+}
