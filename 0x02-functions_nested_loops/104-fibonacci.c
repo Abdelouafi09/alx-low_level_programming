d4fd467c4d4dbb3645db64cdfe7977f1b99a4f25
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-23 03:43:38 +0000

    104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long int fb1, fb2, s;
+
+	fb1 = 0;
+	fb2 = 1;
+	for (i = 0; i < 98; i++)
+	{
+		s = fb1 + fb2;
+		fb1 = fb2;
+		fb2 = s;
+		if (i == 0)
+			printf("%lu", s);
+		else
+			printf(", %lu", s);
+	}
+	printf("\n");
+	return (0);
+}
