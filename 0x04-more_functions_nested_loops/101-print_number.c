ac25b3f5102f598523d29bbd4a0477cdacf5ab61
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-23 23:39:55 +0000

    done

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_number - prints an int
+ * @n: int to print
+ */
+void print_number(int n)
+{
+	int div, d;
+
+	div = 1;
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	while (n / div >= 10)
+	{
+		div *= 10;
+	}
+	while (div > 0)
+	{
+		d = n / div;
+		_putchar(d + '0');
+		n -= d * div;
+		div /= 10;
+	}
+}
