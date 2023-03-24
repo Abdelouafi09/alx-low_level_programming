7cfa044ca90a08d85719c6ac94d5cdd0c24eea72
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-24 23:35:43 +0000

    final 2

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -19,16 +19,18 @@ void print_remaining_days(int month, int day, int year)
 		if (month >= 2 && day >= 60)
 		{
 			day++;
-			rem = 366 - day;
 		}
+		rem = 366 - day;
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", rem);
 
 	}
 	else if (month == 2 && day == 60)
 		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 	else
+	{
 		rem = 365 - day;
-
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", rem);
-
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", rem);
+	}
 }
