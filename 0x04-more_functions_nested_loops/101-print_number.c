75f57d76ff3bf9a5411d6b5a4e1af89f469f6d7a
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-25 05:07:30 +0000

    finally

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,27 +1,20 @@
 #include "main.h"
+
 /**
- * print_number - prints an int
+ * print_number - print int
+ *
  * @n: int to print
  */
 void print_number(int n)
 {
-	int div, d;
-	unsigned int i = n;
+	unsigned int number = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
+		number = -number;
 	}
-	div = 1;
-	while (i / div >= 10)
-	{
-		div *= 10;
-	}
-	while (div > 0)
-	{
-		d = i / div;
-		_putchar(d + '0');
-		i -= d * div;
-		div /= 10;
-	}
+	if ((number / 10) > 0)
+		print_number(number / 10);
+	_putchar(number % 10 + '0');
 }
