61ad1dc702c643d54971aa7a404826cf07dc096c
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-26 22:44:42 +0000

    0104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,16 +7,16 @@
 int main(void)
 {
 	int i;
-	unsigned int fb1, fb2, s;
+	int fb1, fb2, s;
 
 	fb1 = 1;
 	fb2 = 2;
 
-	printf("%u, %u", fb1, fb2);
+	printf("%d, %d", fb1, fb2);
 	for (i = 3; i <= 98; i++)
 	{
 		s = fb1 + fb2;
-		printf(", %u", s);
+		printf(", %d", s);
 		fb1 = fb2;
 		fb2 = s;
 	}
