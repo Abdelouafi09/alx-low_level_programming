6259d0e1dad70410f3cdde5e4c9f73880ea628ef
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-28 00:51:47 +0000

    4th

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - diplay string in reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int len;
+	
+	len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	while (len >= 0)
+	{
+		_putchar(*s);
+		len--;
+		s--;
+	}
+	_putchar('\n');
+}
