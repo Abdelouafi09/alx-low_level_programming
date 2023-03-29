77dd317f1a6e35547b5f158bbad18dedd92a0983
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-29 08:02:38 +0000

    9th

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - copy string
+ * @dest: destination
+ * @src: source
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *s = dest;
+
+	while (*src != '\0')
+	{
+		*s++ = *src++;
+	}
+	*s = '\0';
+	return dest;
+}
