dcf6f9bc5356c92e56421e1d25d8d7c6d41c4ef4
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-31 23:27:29 +0000

    8th

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * leet - change some char with int
+ * @str: string to work on
+ * Return: changed char
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char s0[10] = {'a','A','e','E','o','O','t','T','l','L'};
+	char s1[10] = {'4','4','3','3','0','0','7','7','1','1'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == s0[j])
+				str[i] = s1[j];
+		}
+	}
+	return (str);
+}
