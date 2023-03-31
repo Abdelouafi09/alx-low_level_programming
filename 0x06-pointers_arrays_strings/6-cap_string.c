144d0b5e744b94cf62bac50b42e6ee2f58d11375
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-03-31 20:47:30 +0000

    0007th

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,20 +7,30 @@
  */
 char *cap_string(char *str)
 {
-	int i;
+	int index = 0;
 
-	i = 0;
-	while (str[i])
+	while (str[index])
 	{
-		while (!(str[i] <= 'z' && str[i] >= 'a'))
-			i++;
-		if (str[i - 1] == ' ' || str[i - 1] == ','
-				|| str[i - 1] == ';' || str[i - 1] == '.'
-				|| str[i - 1] == '?' || str[i - 1] == '!' || str[i - 1] == '"'
-				|| str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{'
-				|| str[i - 1] == '}' || str[i - 1] == '\t' || str[i - 1] == '\n')
-			str[i] = str[i] - 32;
-		i++;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}' ||
+				index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
 	return (str);
 }
