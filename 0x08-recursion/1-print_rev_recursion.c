79cf71c25443fed917da9e0056c4fe91d6681ed1
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-04-06 01:21:27 +0000

    1st

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,3 +1,15 @@
 #include "main.h"
+#include <string.h>
 /**
+ * _print_rev_recursion - print in reverse by recursion
+ * @s: string to print
+ *
  */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(++s);
+		_putchar(*s);
+	}
+}
