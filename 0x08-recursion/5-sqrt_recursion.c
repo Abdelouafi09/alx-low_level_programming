723ffa12f8c0273ab714536821bffc5fd5c2d436
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-04-06 02:44:53 +0000

    5th

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,3 +1,45 @@
 #include "main.h"
 /**
+ * square - claculate current square
+ * @n: int
+ * @i: int to raise it with power of 2
+ * Return: square
  */
+int square(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		if (i * i == n)
+			return (i);
+
+		else
+		{
+			return (square(n, i + 1));
+		}
+	}
+}
+
+/**
+ * _sqrt_recursion - calculate the square of n
+ * @n: int to find its square
+ * Return: resulte
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (square(n, 2));
+	}
+}
