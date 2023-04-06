1bfaaff3e106ed2c5800ebc0455f9165b104bc44
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-04-06 01:45:07 +0000

    2nd

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,3 +1,17 @@
 #include "main.h"
 /**
+ * _strlen_recursion - return the length of s
+ * @s: string to work on
+ * Return: length
  */
+int _strlen_recursion(char *s)
+{
+	int c;
+
+	if (*s)
+	{
+		c = 1;
+		c += _strlen_recursion(s + 1);
+	}
+	return (c);
+}
