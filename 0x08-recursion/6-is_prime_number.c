5974c776e962e441c4590a4dae4dd3860554efde
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-04-06 02:53:29 +0000

    6th

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,3 +1,32 @@
 #include "main.h"
+
+int prime(int n, int i);
+
 /**
+ * is_prime_number - check if prime number or not
+ * @n: number to work on
+ *
+ * Return: 1 ==> yes, 0 ==> no
  */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime(n, n - 1));
+}
+
+/**
+ * prime - calculates if a number is prime
+ * @n: work on
+ * @i: interator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (prime(n, i - 1));
+}
