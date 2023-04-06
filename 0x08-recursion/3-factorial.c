4042be5110b0ec7dfe17cee07dc3194d5a7fa1aa
Abdelouafi Er-raqui Loc <abdelwafir@gmail.com>
2023-04-06 02:00:29 +0000

    3rd

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,3 +1,24 @@
 #include "main.h"
 /**
+ * factorial - calculate the factorial
+ * @n: int to work on
+ * Return: resulte
  */
+int factorial(int n)
+{
+	int f;
+
+	if (n < 0)
+	{
+		f = -1;
+	}
+	if (n > 0)
+	{
+		f = n * factorial(n - 1);
+	}
+	else
+	{
+		f = 1;
+	}
+	return (f);
+}
