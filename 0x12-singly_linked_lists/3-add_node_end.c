a4c6dfb67b54f9ef17d9850ae1b4c74081fb6f70
Abdelouafi Er-raqui <abdelwafir@gmail.com>
2023-04-29 23:20:45 +0000

    add new element at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,25 +12,25 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new = malloc(sizeof(list_t));
 
+	if (new == NULL)
+		return (NULL);
+
 	new->str = strdup(str);
 	new->len = strlen(str);
 	new->next = NULL;
 
-	if (new == NULL)
-		return (NULL);
 
 	if (*head == NULL)
 	{
 		*head = new;
+		return (new);
 	}
-	else
+	temp = *head;
+	while (temp->next != NULL)
 	{
-		temp = *head;
-		while (temp->next != NULL)
-		{
-			temp = temp->next;
-		}
+		temp = temp->next;
 	}
+
 
 	temp->next = new;
 	return (new);
