0688161e7fdfc3251921a36be0f27d6cdf7eefba
Abdelouafi Er-raqui <abdelwafir@gmail.com>
2023-05-01 13:38:45 +0000

    print list elements and number of elements [check req solved by respecting betty style in lists.h file] (v4)

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,15 +10,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		if (h->n)
-			printf("%d\n", h->n);
-		else
-			printf("(nil)\n");
-
+		printf("%d\n", h->n);
 		count++;
 		h = h->next;
-
-
 	}
 	return (count);
 }
