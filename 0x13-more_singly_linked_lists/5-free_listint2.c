7f7e455b174d8bdb2301f43a73313cd5c9ab986e
Abdelouafi Er-raqui <abdelwafir@gmail.com>
2023-05-03 01:51:33 +0000

    free the space allocated by list & set head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - free list with setting head to null
+ * @head: head that point to linked list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+	free(head);
+}
