8cfefcdee3546079d7fd7cead22c269eaf5bf006
Abdelouafi Er-raqui <abdelwafir@gmail.com>
2023-05-03 20:13:14 +0000

    OPT: reverse a linked list v2

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,16 +7,16 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
-	listint_t *curr = *head;
 	listint_t *next = NULL;
 
-	curr = *head;
-	while (curr)
+	if (!(*head))
+		return (NULL);
+	while (*head)
 	{
-		next = curr->next;
-		curr->next = prev;
-		prev = curr;
-		curr = next;
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
 	*head = prev;
 	return (*head);
