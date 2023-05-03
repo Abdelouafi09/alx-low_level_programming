cfb124854946225ee42b9600aa9dde2169c0c93d
Abdelouafi Er-raqui <abdelwafir@gmail.com>
2023-05-03 20:08:47 +0000

    OPT: reverse a linked list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverse a linked list
+ * @head: the list pointer
+ * Return: the new list pointer
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *curr = *head;
+	listint_t *next = NULL;
+
+	curr = *head;
+	while (curr)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	*head = prev;
+	return (*head);
+}
