bedf54f34098d6c1299b3767548fd4b5a127137a
Abdelouafi Er-raqui <abdelouafierraqui@gmail.com>
2023-09-29 02:51:49 +0000

    third

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit - print the bit value on the provided position
+ *
+ * @n: the int to search in
+ * @index: the index of the desired bite
+ *
+ * Return: the value of the bite on the provided index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	while (index > 0)
+	{
+		n = n >> 1;
+		index--;
+	}
+	return (n & 1);
+}
