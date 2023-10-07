e476d1ce598f855b0d0b39562763eed121cf12c6
Abdelouafi Er-raqui <abdelouafierraqui@gmail.com>
2023-10-07 03:57:14 +0000

    second

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,8 @@
  * create_file - Creates a file and writes text to it
  * @filename: The name of the file to create
  * @text_content: a string to write to the file
- * Return: 1 on success, -1 on failure
+ * Return: 1 on success,
+ *		-1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -23,8 +24,9 @@ int create_file(const char *filename, char *text_content)
 	{
 		len = 0;
 		while (text_content[len] != '\0')
+		{
 			len++;
-
+		}
 		write_stat = write(fd, text_content, len);
 		if (write_stat == -1)
 		{
