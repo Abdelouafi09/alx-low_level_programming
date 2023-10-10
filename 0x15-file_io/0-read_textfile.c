e7089f982ab1eacb8c8dd836c21c79f63431146a
Abdelouafi Er-raqui <abdelouafierraqui@gmail.com>
2023-10-10 15:43:08 +0000

    final

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,15 +16,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	buf = malloc(letters);
-	if (buf == NULL)
-	{
-		return (0);
-	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		free(buf);
+		return (0);
+	}
+	buf = malloc(letters);
+	if (buf == NULL)
+	{
 		return (0);
 	}
 	bytes_read = read(fd, buf, letters);
@@ -35,11 +34,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-	if (bytes_written == -1)
+	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
 		write(STDOUT_FILENO, "bytes written error\n", 20);
 		return (0);
 
 	}
-	return(bytes_written);
+	free(buf);
+	close(fd);
+	return (bytes_written);
 }
