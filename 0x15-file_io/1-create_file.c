975700b20fc690c8009905317d8c3d007d774c9d
Abdelouafi Er-raqui <abdelouafierraqui@gmail.com>
2023-10-11 13:56:29 +0000

    redo second

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,32 +9,32 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, write_stat, len;
+	ssize_t bytes_written;
+	int length, fd;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	if (text_content != NULL)
+	if (text_content == NULL)
 	{
-		len = 0;
-		while (text_content[len] != '\0')
-		{
-			len++;
-		}
-		write_stat = write(fd, text_content, len);
-		if (write_stat == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		return (1);
 	}
-
+	length = 0;
+	while (text_content[length] != '\0')
+	{
+		length++;
+	}
+	bytes_written = write(fd, text_content, length);
+	if (bytes_written == -1)
+	{
+		return (-1);
+	}
 	close(fd);
 	return (1);
 }
