a0cd32d6d19924ab3bde9bd4d47f7b2d52d602de
Abdelouafi Er-raqui <abdelouafierraqui@gmail.com>
2023-10-11 22:16:16 +0000

    changing the fd int by its unistd.h symbol

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,13 +13,13 @@ void _close(int fd_read, int fd_write)
 	close_stat = close(fd_read);
 	if (close_stat == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_read);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_read);
 		exit(100);
 	}
 	close_stat = close(fd_write);
 	if (close_stat == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_write);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_write);
 		exit(100);
 	}
 }
@@ -38,19 +38,19 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd_read = open(av[1], O_RDONLY);
 	if (fd_read == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	fd_write = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_write == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 	bytes_read = -1;
@@ -59,13 +59,13 @@ int main(int ac, char **av)
 		bytes_read = read(fd_read, buffer, 1024);
 		if (bytes_read == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", av[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
 		bytes_write = write(fd_write, buffer, bytes_read);
 		if (bytes_write == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 	}
