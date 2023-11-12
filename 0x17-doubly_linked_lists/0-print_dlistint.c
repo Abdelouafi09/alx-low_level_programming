7fbfcc24ee65f24f1607949729c85201fa117e5c
Abdelouafi Er-raqui <abdelouafierraqui@gmail.com>
2023-11-12 21:03:04 +0000

    task 0: print a doubly linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - print the doubly linked list
+ * @h: the head pointer
+ * Return: return the number of printed nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+	size_t count = 0;
+
+	curr = h;
+	if (h == NULL)
+		return (count);
+	while (curr != NULL)
+	{
+		printf("%d\n", curr->n);
+		count++;
+		curr = curr->next;
+	}
+	return (count);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+in this directory we will work on learning doubly linked lists
